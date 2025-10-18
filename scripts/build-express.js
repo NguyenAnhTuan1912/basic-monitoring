39d6dc2508a316141febc0ebea37f1dccc500840
Nguyen Anh Tuan <nguyenanhtuan19122002@gmail.com>
2025-10-18 14:46:54 +0700

    Update build script

--- scripts/build-express.js
+++ scripts/build-express.js
@@ -1,11 +1,14 @@
 const path = require("path");
 
+const chalk = require("chalk");
 const esbuild = require("esbuild");
 
 const ROOT_DIR = path.resolve("src/runtimes", "express");
 const ENTRYPOINT = path.resolve(ROOT_DIR, "app.ts");
 const OUT_DIR = path.resolve("build", "express");
 const TSCONFIG_FILE = path.resolve("tsconfig.json");
+
+const start = performance.now();
 
 esbuild
   .build({
@@ -18,4 +21,15 @@ esbuild
 
     external: ["swagger-ui-express", "swagger-ui-dist"],
   })
-  .catch(() => process.exit(1));
+  .then(() => {
+    const duration = performance.now() - start;
+    console.log(
+      "Build time:",
+      chalk.default.yellow((duration / 1000).toFixed(3)),
+      "s",
+    );
+  })
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
